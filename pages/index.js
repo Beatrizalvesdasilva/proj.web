a942e704c5a4dbe6052e0599ee573e2a4535bfd9
Beatriz Alves <ba668031@gmail.com>
2022-09-24 09:18:49 -0300

    Alterando bibliotecas

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles fron '../styles/Home.module.css';
-
  export default function Home () {
 	 return (
 	   <div>
@@ -78,4 +74,5 @@ import styles fron '../styles/Home.module.css';
           </div>
         </div>
       </div>
- )}
+	);
+ }
